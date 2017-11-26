7242378682fe2d612c1c7698395ec79ad05f860b
Kevin161111 <hwha2008@hotmail.com>
2017-11-26 23:37:08 +0800

    sample 9

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
 
 var express = require('express');
 var router = express.Router();
-
-router.get('/posts', function(req, res, next){
-  res.send('hello')
-  res.render('posts', {mytitle: "my posts"});
+//这里下面的get('/')为何这样设计呢，不直接写成get('/posts')
+var postContent = {
+  mytitle: 'my postsss',
+  postsList: ['文章1', '文章2', '文章3']
+}
+router.get('/', function(req, res, next){
+  res.render('posts', postContent);
 });
 
 module.exports = router;
