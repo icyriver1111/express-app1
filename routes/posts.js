69e601658c40742e0e117464b0dde4e88cb02a90
Kevin161111 <hwha2008@hotmail.com>
2017-11-28 02:08:11 +0800

    add postshow

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var router = express.Router();
+var PostModel = require('../models/post')
 //这里下面的get('/')为何这样设计呢，不直接写成get('/posts')
 var postContent = {
   mytitle: 'my postsss',
@@ -12,6 +13,13 @@ router.get('/', function(req, res, next){
 
 router.get('/create', function(req, res, next){
   res.render('create')
+})
+
+router.get('/show', function(req, res, next){
+  var id = req.query.id
+  PostModel.findOne({_id: id},function(err, post){
+    res.render('show', {post})
+  })
 })
 // router.get('/list', function(req, res,next){
 //   // res.json(postContent)
